4565773f3b3b67d008f68a9f52c7af9213d9147e
Andrey <macbook-pro@MacBook-Pro-Andrey.local>
2023-06-20 02:02:32 +0300

    Initial commit

---
+++ Task/TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskApp.swift
+//  Task
+//
+//  Created by Andrey on 20.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
